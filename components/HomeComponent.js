12e09ff6f68e73129c7695a8b80da00f246d6ce0
Antoine EFREI <antoineefrei@Antoine-SANVOISIN.local>
2020-04-23 14:53:09 +0200

    Beginning Structure

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,6 +1,9 @@
 import React, { PureComponent } from 'react';
-import {  View, Text } from 'react-native';
-import { Header } from 'react-native-paper';
+import {  View, StyleSheet, ScrollView, Image, Text } from 'react-native';
+import { Appbar, Searchbar, Avatar, Card, List } from 'react-native-paper';
+import CardView from 'react-native-cardview';
+
+const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
 
 export default class HomeComponent extends PureComponent {
     static navigationOptions = {
@@ -15,8 +18,87 @@ export default class HomeComponent extends PureComponent {
   render() {
     return (
       <View>
-        <Text> HomeComponent </Text>
+        <Appbar.Header>
+            <Appbar.Content
+                title="STONED"
+                // subtitle="Subtitle"
+            />
+            <Appbar.Action 
+                icon="account-circle"
+            />
+        </Appbar.Header>
+        <Searchbar
+            placeholder="Recherche" 
+            value=""
+        />
+        <View>
+            <List.Item
+                title="Premier Type"
+            />
+            <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                <Card style={styles.cards}>
+                    <Card.Title 
+                        title="Card Title"
+                        style={styles.cardHeader}
+                    />
+                    <Card.Cover 
+                        style={styles.cardsImage}
+                        source={{ uri: 'https://picsum.photos/700' }} 
+                    />
+                </Card>
+                <Card style={styles.cards}>
+                    <Card.Title 
+                        title="Card Title"
+                        style={styles.cardHeader}
+                    />
+                    <Card.Cover
+                        style={styles.cardsImage}
+                        source={{ uri: 'https://picsum.photos/700' }} 
+                    />
+                </Card>
+                <Card style={styles.cards}>
+                    <Card.Title 
+                        title="Card Title"
+                        style={styles.cardHeader}
+                    />
+                    <Card.Cover
+                        style={styles.cardsImage}
+                        source={{ uri: 'https://picsum.photos/700' }} 
+                    />
+                </Card>
+                <Card style={styles.cards}>
+                    <Card.Title 
+                        title="Card Title"
+                        style={styles.cardHeader}
+                    />
+                    <Card.Cover
+                        style={styles.cardsImage}
+                        source={{ uri: 'https://picsum.photos/700' }} 
+                    />
+                </Card>
+            </ScrollView>
+        </View>
       </View>
     );
   }
 }
+const styles = StyleSheet.create({
+    cardsGroup: {
+        flex: 1,
+        flexDirection: 'row'
+    },
+    cards: {
+        width: 190,
+        height: 200,
+        marginLeft: 10
+    },
+    cardsHeader: {
+        backgroundColor: '#ff00ff',
+        justifyContent: 'center',
+        height: 10
+    },
+    cardsImage:{
+        borderRadius: 5,
+        height: 190
+    }
+});
---
+++ components/user/MenuComponent.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {  View, Text } from 'react-native';
+
+class MenuComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> componentText </Text>
+      </View>
+    );
+  }
+}
+
+export default MenuComponent;
